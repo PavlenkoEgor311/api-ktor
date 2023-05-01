a76b9788d5c63b3cbea5b742d7e79f72a37a61e8
PavlenkoEgor311 <pavlenkoegor2@gmail.com>
2023-05-01 11:59:59 +0400

    delete

--- index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const { createServer } = require('http');
-const { default: app } = require('./build/server/server');
-
-exports.api = functions.https.onRequest((req, res) => {
-    createServer(app).listen(8080, () => {
-        console.log('Server listening on port 8080');
-    });
-});
